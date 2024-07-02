78937f37331bc1692fc387b67b1427d6babe32db
vkletsko <v.kletsko@gmail.com>
2024-07-02 19:48:03 +0300

    init commit

--- src/2.ts
+++ src/2.ts
@@ -16,24 +16,30 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {
+    this.increaseSalary();
   }
 
-  getEmployeeDetails() {
+  getEmployeeDetails(): string {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  }
+  increaseSalary(this: Employee): void {
+    this.salary += 10_000;
   }
 }
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+  }
 }
 
+const manager = new Manager('Mango', 'clouds', 10_000);
 
+console.log('managerDetails:', manager.getEmployeeDetails());
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(this: Key): number {
+    return this.signature;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  public abstract openDoor(key: number): void;
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): number {
+    return this.key.getSignature();
+  }
+}
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(keySignature: number): void {
+    if (this.key.getSignature() === keySignature) {
+      this.door = true;
+      console.log(`Двері відчинені`);
+    } else {
+      console.log(`Двері зачинені`);
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +57,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
